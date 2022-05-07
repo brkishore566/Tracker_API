8ff6e9eb8c2cd550a856d24545d61e67532282d2
Kishore B R <47842664+brkishore566@users.noreply.github.com>
2022-05-07 22:38:47 +0530

    Update simulator.py

--- simulator.py
+++ simulator.py
@@ -7,9 +7,8 @@ import json
 url = "http://127.0.0.1:5000/vitals_input"
 
 Patient = { "user_id": 123, "timestamp": "", "heart_rate" : 45, "respiration_rate" : 18, "activity" : 3 }
-i = 0
 user_id = 123
-while True and i != 10:
+while True:
     heart_rate = random.randint(60,100)
     resp_rate =  random.randint(16,20)
     activity = random.randint(1,3)
